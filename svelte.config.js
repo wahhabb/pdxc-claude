1e9ff99c0f70e7a1d2f81b1371d369874a3ad7c8
Wahhab Baldwin <wahhabb@gmail.com>
2026-02-07 22:25:58 -0800

    Handle enhancedImaages build error

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 
 /** @type {import('@sveltejs/kit').Config} */
